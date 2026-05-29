be8df75abd3cc3bc76cb7641b7bb8fb63a058278
Mathias Strømberg Durkis <71726326+msdurk@users.noreply.github.com>
2026-05-29 11:34:45 +0200

    Delete phishing_detection directory

--- phishing_detection/ai_agent/llama_test.py
+++
@@ -1,8 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
-
-folder = "/fp/projects01/ec12/mathisdu/lllama/Llama-3.1-8B-Instruct"
-
-tok = AutoTokenizer.from_pretrained(folder, use_fast=True)
-model = AutoModelForCausalLM.from_pretrained(folder, device_map="auto")
-
-print("Model loaded successfully!")
